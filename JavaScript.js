042d15630982d9732b7882c1b694739d537d9e40
Amresh Thakur <94734572+amreshthakur@users.noreply.github.com>
2023-11-07 00:08:16 +0545

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,80 @@
+    
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add("active-tab");
+};
+
+
+
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topButton").style.display = "block";
+    } else {
+        document.getElementById("topButton").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+
+//==========================================================================//
+
+
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const searchResults = document.getElementById("search-results");
+
+searchButton.addEventListener("click", () => {
+    const query = searchInput.value.trim();
+
+    // You can perform a search or filter operation here based on the query
+    // For this example, let's display a sample result
+    const result = "Search result for: " + query;
+
+    // Display the search result
+    searchResults.style.display = "block";
+    searchResults.innerHTML = result;
+});
+
+
+
+
+//========================================================
+
+  window.addEventListener('keydown', function (e) {
+    if (e.keyCode === 85 && e.ctrlKey) {
+      // Check if the user presses Ctrl+U (view source)
+      alert("Viewing the source code is not allowed.");
+      e.preventDefault();
+    }
+  });
+
+
+
+
+
+  window.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+  });
+
